2f549ebbf4e037538f34b14e81c69c2c6005cbf2
Dominik Domanski <dominik.d@syte-vc.com>
2019-08-10 20:22:26 +0300

    semantic page strucuture explained

---
+++ main.js
@@ -0,0 +1,5 @@
+// alert('Main.js is working')
+
+const testButton = document.querySelector('#test-button')
+console.log(testButton)
+testButton.addEventListener('click', () => console.log('Test button clicked'))
