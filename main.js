dbe406524abc6e0958ad2cbe8c9a23c87980f660
Dominik Domanski <dominik.d@syte-vc.com>
2019-08-17 20:35:09 +0300

    initial commit

--- main.js
+++
@@ -1,5 +0,0 @@
-// alert('Main.js is working')
-
-// const testButton = document.querySelector('#extreme')
-// console.log(testButton)
-// testButton.addEventListener('click', () => console.log('Test button clicked'))
